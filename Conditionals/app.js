abfb9eed5a200f4d6e6cd254dce121ba63fd82f2
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-15 21:53:36 -0800

    Updates to Conditionals/app.js
    +Math.random generator to differentiate between numbers less than or greater than 0.5 using if and else statements
    +dayOfWeek finder that prints a string based on one of three declared days or any other input using less than or equal to random, a user input prompt and if, else if and else statements
    +age finder that prints a string based on one of 3 declared increasing number ranges using less than, if, else if and else statements

---
+++ Conditionals/app.js
@@ -0,0 +1,31 @@
+// let random = Math.random();
+// if (random <= 0.5) {
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+// } else {
+//   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+// }
+// console.log(random);
+
+// const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
+
+// if (dayOfWeek === "monday") {
+//   console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "saturday") {
+//   console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === "friday") {
+//   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//   console.log("WHY IS IT THIS DAY??!");
+// }
+
+const age = 89;
+
+if (age < 5) {
+  console.log("You are a baby. You get in for free.");
+} else if (age < 10) {
+  console.log("You are a child. Gimme your chore monies.");
+} else if (age < 65) {
+  console.log("You are an adult. Your paycheck is mine.");
+} else {
+  console.log("You are a senior. You get an old discount!");
+}
