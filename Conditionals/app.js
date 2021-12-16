92479cc1395816ada92247c275ccb60a5c891c6e
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-15 22:27:15 -0800

    Updates to Conditionals/app.js
    +Notes for each of the script sections of content written prior
    +Password prompt with user input prompt using nested if statements and an else statement, printed as a warning if the if statement conditions are not met
    +Commented out everything but password prompt

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -1,3 +1,5 @@
+// Random number generator between 0 and 1, not including those numbers, using if and else statements
+
 // let random = Math.random();
 // if (random <= 0.5) {
 //   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
@@ -5,6 +7,8 @@
 //   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
 // }
 // console.log(random);
+
+// Day of the week finder to print opinions of days, entered via user prompt, using if, else if and else statements
 
 // const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
 
@@ -18,14 +22,28 @@
 //   console.log("WHY IS IT THIS DAY??!");
 // }
 
-const age = 89;
+// Age finder to print ticket prices using if, else if and else statements
 
-if (age < 5) {
-  console.log("You are a baby. You get in for free.");
-} else if (age < 10) {
-  console.log("You are a child. Gimme your chore monies.");
-} else if (age < 65) {
-  console.log("You are an adult. Your paycheck is mine.");
+// const age = 89;
+
+// if (age < 5) {
+//   console.log("You are a baby. You get in for free.");
+// } else if (age < 10) {
+//   console.log("You are a child. Gimme your chore monies.");
+// } else if (age < 65) {
+//   console.log("You are an adult. Your paycheck is mine.");
+// } else {
+//   console.log("You are a senior. You get an old discount!");
+// }
+
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Password validated!");
+  } else {
+    console.warn("Password cannot contain spaces.");
+  }
 } else {
-  console.log("You are a senior. You get an old discount!");
+  console.warn("Password too short. Must contain at least 6 characters.");
 }
