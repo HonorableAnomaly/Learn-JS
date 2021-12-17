01c721d8f2cf0291f478bf43ce4eb1ada38a4cc6
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-16 18:31:39 -0800

    Updates
    +Commented out password prompt that utilizes &&
    +Ticket discount generator based on user input prompt that utilizes && and ||

--- Logical-Operators/app.js
+++ Logical-Operators/app.js
@@ -1,7 +1,19 @@
-const password = prompt("Enter your password");
+// const password = prompt("Enter your password");
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-  console.log("Password validated!");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Password validated!");
+// } else {
+//   console.warn("Incorrect format for password");
+// }
+
+const age = -10;
+
+if ((age > 0 && age <= 5) || age >= 65) {
+  console.log("FREE!");
+} else if (age > 5 && age <= 10) {
+  console.log("$10");
+} else if (age > 10 && age < 65) {
+  console.log("$20");
 } else {
-  console.warn("Incorrect format for password");
+  console.warn("INVALID AGE");
 }
