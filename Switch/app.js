aa6cc4ae77e7bb9f6782bea0326a68f4813b5178
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-16 19:09:34 -0800

    Update app.js
    +Commented out if, else if and else statements for the day evaluator
    +Used a switch for the day evaluator with 7 cases, one for each day of the week, and a default case for anything that does not fall between 1 and 7.

--- Switch/app.js
+++ Switch/app.js
@@ -1,15 +1,42 @@
+// Day evaluator using a switch
 const day = 2;
 
-if (day === 1) {
-  console.log("Monday");
-} else if (day === 2) {
-  console.log("Tuesday");
-} else if (day === 3) {
-  console.log("Wednesday");
-} else if (day === 4) {
-  console.log("Thursday");
-} else if (day === 5) {
-  console.log("Friday");
-} else {
-  console.log("Weekend!");
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  // You can run multiple cases in the same break
+  case 6:
+  case 7:
+    console.log("Weekend!");
+    break;
+
+  default:
+    console.log("Whaaaa?!");
 }
+
+// if (day === 1) {
+//   console.log("Monday");
+// } else if (day === 2) {
+//   console.log("Tuesday");
+// } else if (day === 3) {
+//   console.log("Wednesday");
+// } else if (day === 4) {
+//   console.log("Thursday");
+// } else if (day === 5) {
+//   console.log("Friday");
+// } else {
+//   console.log("Weekend!");
+// }
