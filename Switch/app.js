fa6f4d9fe2b8792e1a9e360644c8d36c184f3296
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-16 18:54:33 -0800

    New Learn-JS folder, files and content
    New folder:
    +Switch
    
    New files:
    +index.html
    +app.js
    
    New content (app.js):
    +Day of the week evaluator using if, else if and else statements

---
+++ Switch/app.js
@@ -0,0 +1,15 @@
+const day = 2;
+
+if (day === 1) {
+  console.log("Monday");
+} else if (day === 2) {
+  console.log("Tuesday");
+} else if (day === 3) {
+  console.log("Wednesday");
+} else if (day === 4) {
+  console.log("Thursday");
+} else if (day === 5) {
+  console.log("Friday");
+} else {
+  console.log("Weekend!");
+}
