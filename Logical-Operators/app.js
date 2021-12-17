eee2317fa80cd23b813ac060ba85277bc18fa98a
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-16 18:37:21 -0800

    Update app.js
    +Commented out ticket price generator using age user input prompt
    +First name user input prompt and an if statement to detect when input was not entered to initiate another prompt

--- Logical-Operators/app.js
+++ Logical-Operators/app.js
@@ -6,14 +6,20 @@
 //   console.warn("Incorrect format for password");
 // }
 
-const age = -10;
+// const age = -10;
 
-if ((age > 0 && age <= 5) || age >= 65) {
-  console.log("FREE!");
-} else if (age > 5 && age <= 10) {
-  console.log("$10");
-} else if (age > 10 && age < 65) {
-  console.log("$20");
-} else {
-  console.warn("INVALID AGE");
+// if ((age > 0 && age <= 5) || age >= 65) {
+//   console.log("FREE!");
+// } else if (age > 5 && age <= 10) {
+//   console.log("$10");
+// } else if (age > 10 && age < 65) {
+//   console.log("$20");
+// } else {
+//   console.warn("INVALID AGE");
+// }
+
+const firstName = prompt("Enter your first name");
+
+if (!firstName) {
+  prompt("Please enter your first name");
 }
