5804ed5b444619fe4d097ac36310a235b33d4ccd
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-24 13:21:30 -0800

    Update app.js
    +For loop with the variable i, incrementing by 1 until 10, printing i
    +Commented out for loop
    +For loop with the variable i, incrementing by 2 until 20, printing i to demo even numbers
    +Commented out
    +For loop with the variable i, incrementing by 10 from 100 until 0, printing i
    +Commented out
    +For loop with the variable i, incrementing by 10* from 10 to 1000, printing i

--- Loops/For-Loops/app.js
+++ Loops/For-Loops/app.js
@@ -10,9 +10,9 @@
 // console.log(10);
 
 // First expression is a variable, second is a boolean to check whether the loop continues, and the third is the increment expression
-for(let i = 1; i <= 10; i++){
-    console.log(i);
-}
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
 
 // Another way to write for loops: different variable name, different print result and += 1 instead of ++
 
@@ -21,3 +21,18 @@ for(let i = 1; i <= 10; i++){
 //     console.log(num);
 // }
 
+// For loop printing even numbers
+// Change the starting number to 2 to skip 0, start at 1 to do odd numbers
+// for(let i = 0; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+// for loop counting down from 100 to 0 in increments of 10
+// for(let i = 100; i >= 0; i -= 10){
+//     console.log(i);
+// }
+
+// Odd starting number, printing after multiplication increment times 10
+for(let i = 10; i <= 1000; i *= 10){
+    console.log(i);
+}
