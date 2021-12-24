c7b0774f74f65999ac1f40b95b0f514c14f8a27e
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-24 13:00:20 -0800

    Updates
    +Printed numbers 1-10 using console.log and commented them out as demo for for loops over manual entry
    +For loop with generic variable i, incremented positively by 1, iterated until printing 10
    +Another variant of the for loop using a named variable, a printed result as a string rather than a number and using += 1 instead of ++ as the increment expression

---
+++ Loops/For-Loops/app.js
@@ -0,0 +1,23 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// First expression is a variable, second is a boolean to check whether the loop continues, and the third is the increment expression
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// Another way to write for loops: different variable name, different print result and += 1 instead of ++
+
+// for(let num = 1; num <= 10; num += 1){
+//     console.log("I AM IN THE LOOP BODY!!!")
+//     console.log(num);
+// }
+
