627d9297e3bfa25c0542cd995b338d76b9208987
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-31 15:59:28 -0800

    Update app.js
    +commented out for of loop foods
    +pasted in nested for loops and const variable array seating Assignment
    +commented out nested for loops
    +nested for of loops declaring same functions as nested for loops with much shorter syntax and easier to read, keeping let variable i as row and re-identifying let variable j as student

--- Loops/For-Of-Loops/app.js
+++ Loops/For-Of-Loops/app.js
@@ -8,6 +8,29 @@ const foods = ["cheese", "apples", "beef", "tortilla"];
 
 // A for of loop to do the same!
 
-for (let food of foods) {
-  console.log(`See recipes at food.com/${food}`);
+// for (let food of foods) {
+//   console.log(`See recipes at food.com/${food}`);
+// }
+
+const seatingAssignment = [
+  ["Madisen", "Kimmy", "Trevor"],
+  ["Josh", "Becca", "Arely", "Isaac"],
+  ["Mitchell", "Kenleigh", "Mara", "Brittany"],
+];
+
+// Copied nested for loops from For-Loops folder
+
+// for (let i = 0; i < seatingAssignment.length; i++) {
+//   const row = seatingAssignment[i];
+//   console.log(`Row #${i + 1}`);
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(row[j]);
+//   }
+// }
+
+// Changing the nested for loops above to nested for of loops!!!
+for (let row of seatingAssignment) {
+  for (let student of row) {
+    console.log(student);
+  }
 }
