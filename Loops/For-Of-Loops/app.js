6ad5958d4455bd7dc87182f657d92b72445ae744
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-12-31 15:51:39 -0800

    Updates and fixes
    Fixes:
    +changed elements in const variable foods to strings
    
    Updates:
    +for loop iterating across array foods, printing statement literal including each element
    +commented out
    +for of loop declaring new let variable for each element, printing statement literal including each element

--- Loops/For-Of-Loops/app.js
+++ Loops/For-Of-Loops/app.js
@@ -1,1 +1,13 @@
-const foods = [cheese, apples, beef, tortilla];
+const foods = ["cheese", "apples", "beef", "tortilla"];
+
+// A for loop to iterate across array foods
+
+// for (let i = 0; i < foods.length; i++) {
+//   console.log(`See recipes at food.com/${foods[i]}`);
+// }
+
+// A for of loop to do the same!
+
+for (let food of foods) {
+  console.log(`See recipes at food.com/${food}`);
+}
