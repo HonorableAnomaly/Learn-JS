b5a1a90a4108d6085804e4f2de9bfc1b70f46f74
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-04 19:53:00 -0800

    Update app.js
    +commented out let variable die1
    +function singSong that prints 3 separate lines 'DO', 'RE' and 'MI'

--- Basics/app.js
+++ Basics/app.js
@@ -1,1 +1,7 @@
-let die1 = Math.floor(Math.random() * 6) + 1;
+// let die1 = Math.floor(Math.random() * 6) + 1;
+
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
