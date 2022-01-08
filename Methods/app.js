f16d981af3998ce09b617d7e2a2270fdf893e764
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-08 11:00:54 -0800

    Update app.js
    +const object myMath containing PI, function square with argument num returning num times num and function cube with argument num returning num to 3
    +commented out const object myMath
    +rewrote const object myMath with shorter syntax to remove the word 'function' from the inner factory functions

---
+++ Methods/app.js
@@ -0,0 +1,20 @@
+// const myMath = {
+//   PI: 3.14159,
+//   square: function (num) {
+//     return num * num;
+//   },
+//   cube: function (num) {
+//     return num ** 3;
+//   },
+// };
+
+// more efficient way to write method functions is to remove the word 'function' from the object
+const myMath = {
+  PI: 3.14159,
+  square(num) {
+    return num * num;
+  },
+  cube(num) {
+    return num ** 3;
+  },
+};
