838d90c521880cfc400e89c367c92c1fcac7a636
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-07 21:42:41 -0800

    Update app.js
    +commented out bird functions
    +let variable radius
    +if conditional containing const variable PI and let variable msg
    +printing radius and msg to demo scoping in blocks
    +for loop containing let variable msg, printing msg
    +printing msg and i using var keyword compared to let and const to demo var not being block scoped

--- Scope/app.js
+++ Scope/app.js
@@ -7,10 +7,27 @@
 // collectEggs();
 // console.log(totalEggs);
 
-let bird = "Scarlet Macaw";
-function birdWatch() {
-  let bird = "Great Blue Heron";
-  console.log(bird);
-}
+// let bird = "Scarlet Macaw";
+// function birdWatch() {
+//   let bird = "Great Blue Heron";
+//   console.log(bird);
+// }
 
-birdWatch();
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//   const PI = 3.14159;
+//   let msg = "Hiii!";
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+// var can be used to access variables inside blocks, while let and const are block scoped
+for (let i = 0; i < 5; i++) {
+  let msg = "esdangtorewnbt";
+  console.log(msg);
+}
+console.log(msg);
+console.log(i);
