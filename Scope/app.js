bf2c35fd9f61d22bd54fbbd9d82019493ec11ef8
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-07 21:28:07 -0800

    Update app.js
    +commented out eggs printing
    +let variable bird
    +function birdWatch containing another let variable bird, printing bird
    +calling function birdWatch to demo function scope priority with same variable names

--- Scope/app.js
+++ Scope/app.js
@@ -6,3 +6,11 @@
 // console.log(totalEggs);
 // collectEggs();
 // console.log(totalEggs);
+
+let bird = "Scarlet Macaw";
+function birdWatch() {
+  let bird = "Great Blue Heron";
+  console.log(bird);
+}
+
+birdWatch();
