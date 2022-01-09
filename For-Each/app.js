82682485ccd6189406022d3c37bde0041036ea48
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-08 17:08:02 -0800

    Update app.js
    +const variable numbers containing an array of 20 numbers
    +function print with element argument that prints the element
    +two print for the index of numbers to demo manual entry
    +numbers.forEach passing in print, printing each index
    +numbers.forEach passing in a function with an el argument, containing an if statement el modulo 2 that compares to 0, printing el to demo passing in functions with arguments as methods
    +for loop with let variable el of const variable numbers, printing el accomplishes the same thing in this instance

---
+++ For-Each/app.js
@@ -0,0 +1,21 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// function print(element) {
+//   console.log(element);
+// }
+
+// // We can manually print each element from the array, but forEach does it for us.
+// // print(numbers[0]);
+// // print(numbers[1]);
+// numbers.forEach(print);
+
+// numbers.forEach(function (el) {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
+
+// this is the same thing in this case, but forEach can accomplish a lot with more complex functions, and this for loop doesn't use functions
+// for (let el of numbers) {
+//   console.log(el);
+// }
