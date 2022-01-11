0cec06200c92a9b8a61229206cee7dbdd3843dce
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-10 21:45:22 -0800

    Update app.js
    +commented out const variable add and arrow function add
    +const square num with an arrow function to return num squared
    +commented out const variable rollDie with arrow function, returning 6-sided die roll
    +const variable rollDie with no argument using an arrow function to return the roll of a 6-sided die
    +const variable add with an arrow function that accepts two arguments a and b and returns a + b to demo single-line syntax with no parens
    +const variable array movies

---
+++ Functions/Arrow-Functions/app.js
@@ -0,0 +1,56 @@
+// const add = function (x, y) {
+//     return x + y;
+// }
+
+// The new arrow style of writing functions! Removes the keyword function
+// const add = (x, y) => {
+//     return x + y;
+// };
+
+// If there is only a single argument that goes into the arrow function, you can remove the parens!
+const square = num => {
+    return num ** 2;
+};
+
+// If there are no arguments that go into the arrow function, you still need the parens
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+// You can make arrow function syntax even shorter if you use implicit return, by turning the curlys into parens and removing the return keyword and semicolons
+// Implicit return will only work if you have a single expression inside the parens
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+// This syntax is the same as the implicit return but done on one line and subtracting the parens
+const add = (a, b) => a + b
+
+const movies = [
+    {
+      title: "The Matrix",
+      score: 93,
+    },
+    {
+      title: "Toy Story",
+      score: 97,
+    },
+    {
+      title: "Finding Nemo",
+      score: 86,
+    },
+    {
+      title: "Lord of the Rings",
+      score: 99,
+    },
+    {
+      title: "Scream",
+      score: 73,
+    },
+    {
+      title: "Love, Actually",
+      score: 88,
+    },
+  ];
+
+  
