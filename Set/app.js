35e916a8a02ef209c11e56d39ed53567476ea194
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-12 20:31:00 -0800

    Update app.js
    +commented out printed greeting, setTimeout and printed farewell
    +const variable id setInterval with a blank arrow function that prints a random number every 2 seconds to demo a stored value with a setInterval that can be stopped with clearInterval

--- Set/app.js
+++ Set/app.js
@@ -1,8 +1,16 @@
-console.log("HELLO!!!");
+// console.log("HELLO!!!");
 
-// setTimeout adds a delay to the function before it executes
-setTimeout (() => {
-    console.log("...are you still there?");
-}, 3000);
+// // setTimeout adds a delay to the function before it executes
+// setTimeout (() => {
+//     console.log("...are you still there?");
+// }, 3000);
 
-console.log("GOODBYE!!!");
+// console.log("GOODBYE!!!");
+
+// same syntax footprint as setTimeout, but executes once every interval
+const id = setInterval (() => {
+    console.log(Math.random());
+}, 2000);
+
+// typing clearInterval will stop the interval from running
+// clearInterval(id);
