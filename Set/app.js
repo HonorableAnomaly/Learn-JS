60c3183d16db0fab05c92493c582d3208a17d28d
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-12 20:20:17 -0800

    New folder, files and content
    New folder:
    +Set
    
    New files:
    +index.html
    +app.js
    
    New content (app.js):
    +printing greeting
    +setTimeout to delay printing a question after 3 seconds
    +printing farewell after to demo that it prints immediately with the greeting

---
+++ Set/app.js
@@ -0,0 +1,8 @@
+console.log("HELLO!!!");
+
+// setTimeout adds a delay to the function before it executes
+setTimeout (() => {
+    console.log("...are you still there?");
+}, 3000);
+
+console.log("GOODBYE!!!");
