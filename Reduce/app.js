22bd2a05b2e19b66988a6bd39a96579d34278e63
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-13 21:02:35 -0800

    updates
    +const variable array prices containing 5 numbers
    +let variable total = 0
    +for loop containing let variable price calling const variable prices, returning the new total after adding each price
    +printing total
    
    +const variable total is const variable prices reduced using the arguments accumulator total and each price and arrow function, returning total plus price
    
    +the same syntax as above but with an implicit return
    
    +const variable minPrice is const variable prices reduced using the arguments accumulator min and each price with an arrow function containing an if statement price less than min, returning price, otherwise returning min
    
    +the same syntax as above but finding the maxPrice and min is replaced with max
    
    +const variable array movies copied from Some-Every lesson
    +const variable highestRated is const variable array movies reduced using the arguments accumulator bestMovie and each currMovie with an arrow function containing an if statement currMovie score is greater than bestMovie score, returning currMovie to find the highest rated movie in the array
    +same syntax as above but for the lowestRated movie
    
    +const variable array evens containing 4 numbers
    +const variable add is const variable evens reduced with the arguments accumulator sum and each num with an arrow function and implicit return sum adding num, and an additional argument after the implicit return to serve as the initial reduce value

--- Reduce/app.js
+++ Reduce/app.js
@@ -1,1 +1,88 @@
 const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+// Finds the sum of the array
+
+// let total = 0;
+// for (let price of prices) {
+//     total += price;
+// };
+// console.log(total);
+
+// The sum of the array using the reduce method
+// const total = prices.reduce ((total, price) => {
+//     return total + price;
+// })
+
+// Same reduce method with implicit return
+const total = prices.reduce ((total, price) => total + price);
+
+// Using reduce method to find the lowest priced item in the array
+const minPrice = prices.reduce((min, price) => {
+    if (price < min) {
+        return price;
+    }
+    return min;
+})
+
+// Using reduce method to find the highest priced item in the array
+const maxPrice = prices.reduce ((max, price) => {
+    if (price > max) {
+        return price;
+    }
+    return max;
+})
+
+
+const movies = [
+    {
+      title: "The Matrix",
+      score: 93,
+      year: 1999
+    },
+    {
+      title: "Toy Story",
+      score: 97,
+      year: 1986
+    },
+    {
+      title: "Finding Nemo",
+      score: 86,
+      year: 1938
+    },
+    {
+      title: "Lord of the Rings",
+      score: 99,
+      year: 2006
+    },
+    {
+      title: "Scream",
+      score: 73,
+      year: 2013
+    },
+    {
+      title: "Love, Actually",
+      score: 88,
+      year: 1992
+    },
+];
+
+// Using reduce method to find the best score of movies in the object array
+const highestRated = movies.reduce ((bestMovie, currMovie) => {
+    if (currMovie.score > bestMovie.score) {
+        return currMovie;
+    }
+    return bestMovie;
+})
+
+// Using reduce method to find the worst score of movies in the object array
+const lowestRated = movies.reduce ((worstMovie, currMovie) => {
+    if (currMovie.score < worstMovie.score) {
+         return currMovie;
+    }
+    return worstMovie;
+})
+
+
+const evens = [2, 4, 6, 8];
+// Introducing a second argument after the implicit return that reduce is added to, serving as the initial value of the reduce
+const added = evens.reduce ((sum, num) => sum + num, 100);
