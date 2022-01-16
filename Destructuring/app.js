08ebcb25649be88295c70173252f81f6e25b3e9c
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-16 12:16:55 -0800

    Update app.js
    +key-value pair magic with a value of healing within const variable object user
    +const variable object user2 containing 9 key-value pairs
    +const variable object equal to const variable object user destructured with 2 keys redefined as custom variables, one being given a default value to demo that it does not overwrite the original value in the object it was copied from
    +const variable object equal to const variable object user destructured with 3 keys redefined as custom variables, one not existing and given a default value

--- Destructuring/app.js
+++ Destructuring/app.js
@@ -18,5 +18,33 @@ const user = {
     died: 'FO 120',
     bio: 'Legolas Greenleaf is the elf prince of the Woodland Realm in Middle Earth',
     province: 'Ithilien',
-    realm: 'Woodland'
+    realm: 'Woodland',
+    magic: 'Healing'
 };
+
+const user2 = {
+    email: 'aragorn@gmail.com',
+    password: 'Ar@g0rn',
+    firstName: 'Aragorn',
+    lastName: 'Elessar',
+    born: 'TA 2931',
+    died: 'FO 120',
+    bio: 'Aragorn, son of Arathron, is the rightful King of Minas Tirith and the realm of men in Middle Earth',
+    province: 'Minas Tirith',
+    realm: 'Reunited Kingdom'
+};
+
+// Old way to pull out key-value pairs into a new variable
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+// const fullName = firstName + lastName;
+
+// Destructuring allows us to copy specific key-value pairs in any order and assign them to new variables
+// const {email, firstName, lastName, province, bio} = user;
+
+// Renaming the variable as we copy it over into that new variable
+// Default values provided in the new variable assignment do not overwrite existing key-value pair values
+const {born: birthYear, died: deathYear = 45} = user;
+
+// A default value can be given to the new variables as they are destructured in case there is no key-value pair present
+const {province, realm, magic = 'N/A'} = user2;
