b00d1cf818b67a61f8fa81f47163061f2dc52794
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-19 19:38:15 -0800

    Updates to index and app.js
    Updates (index.html):
    +script link to app.js
    
    Updates (app.js):
    +const variable allImages that gets every element by tag name img
    +for loop img of allImages that changes the img src to the same one
    +commented out
    
    +const variable allHeadlines that gets every element by class name for headlines
    +for loop headline of allHeadlines that changes their innerText to read the same thing
    +commented out
    
    +const variable links that query selects all anchor tags within paragraphs
    +for loop link of links that prints each href
    +commented out

---
+++ The-DOM/app.js
@@ -0,0 +1,20 @@
+// First manipulation of html through JS!!!
+// const allImages = document.getElementsByTagName('img')
+
+// for(let img of allImages) {
+//     img.src = 'http://upload.wikimedia.org/wikipedia/en/thumb/9/91/WoW_Box_Art1.jpg/220px-WoW_Box_Art1.jpg'
+// }
+
+// Getting elements by class name and iterating over them to change their innerText
+// const allHeadlines = document.getElementsByClassName('mw-headline')
+
+// for(let headline of allHeadlines) {
+//     headline.innerText = 'HACKED!'
+// }
+
+// Selecting all anchor tags nested in paragraphs and printing each of their hrefs
+// const links = document.querySelectorAll ('p a');
+
+// for (let link of links) {
+//     console.log(link.href)
+// }
