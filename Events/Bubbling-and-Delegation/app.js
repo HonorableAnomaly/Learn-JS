bbe6dc115b96c4506426adcf72e8c7fa5f2edde5
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-01-27 20:08:48 -0800

    Updates to 2 files
    index.html
    +grocery list code copied from another lesson
    
    app.js
    +grocery list code from another lesson
    +const variable lis that searches for all lis
    +for loop using argument let variable li of lis containing a click event listener on li that removes that li to demo that this only works for lis created before the page initiates
    +commented out
    +click event listener added to list using the event argument through an arrow function that event targets the nodeName for li and removes that event target

--- Events/Bubbling-and-Delegation/app.js
+++ Events/Bubbling-and-Delegation/app.js
@@ -18,3 +18,37 @@ const makeRandColor = () => {
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 };
+
+// Form events code from the grocery list lesson to demo delegation
+const form = document.querySelector("form");
+const product = document.querySelector("#product");
+const qty = document.querySelector("#qty");
+const list = document.querySelector("#list");
+
+// Added for the remove li event listener
+
+// Only works for any li that are in place already, not those that are added on the page, because the page isn't refreshing
+// const lis = document.querySelectorAll("li");
+// for (let li of lis) {
+//   li.addEventListener("click", () => {
+//     li.remove();
+//   });
+// }
+
+// Adding a target on the nodeName ensures that what was clicked was a target that was intended rather than another node or element
+list.addEventListener("click", (e) => {
+  e.target.nodeName === "LI" && e.target.remove();
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const productName = product.value;
+  const qtyAmount = qty.value;
+  const newItem = document.createElement("li");
+  newItem.innerText = `${qtyAmount} ${productName}`;
+  //   list.insertAdjacentElement(beforeend, newItem);
+  //   list.parentElement.appendChild(newItem);
+  list.append(newItem);
+  product.value = "";
+  qty.value = "";
+});
