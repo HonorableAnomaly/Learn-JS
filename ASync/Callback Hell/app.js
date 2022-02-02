880ab9d93caff0819885e45faf81d9399c99d7f8
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-01 18:43:27 -0800

    Updates to 2 files
    index.html
    +html style for the body to transition whenever the background color changes at 1s intervals
    
    app.js
    +7 nested setTimeout callback functions that change the body's background color, each set to 1 second
    +commented out
    +The same type of system, but refactored using a variable with arguments and a single setTimeout function that changes the background color with a doNext uncalled and called version to push the next iteration
    +The variable calls have been refactored into better nested functions that call each of the arguments individually
    +A non-functional version of an API search via a function with an argument and an empty arrow functioncontaining another function that takes an argument and an empty arrow function, containing some line of code for successful retrieval or skipping to another empty arrow function that contains a line of code for failure of retrieval. If neither of those happens, another empty arrow function is initiated that would run instead if the API is down or the request fails for some other reason

---
+++ ASync/Callback Hell/app.js
@@ -0,0 +1,61 @@
+// Nested setTimeout callbacks within a function
+
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                     setTimeout(() => {
+//                         document.body.style.backgroundColor = 'indigo';
+//                         setTimeout(() => {
+//                             document.body.style.backgroundColor = 'violet';
+//                         }, 1000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+// A variable taking several arguments for the same setTimeout function
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay);
+};
+
+// Rewritten function to take the color change variable, still nested, but better refactored
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('green', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+                    delayedColorChange('indigo', 1000, () => {
+                        delayedColorChange('purple', 1000, () => {
+    
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+
+// demo typical simple async callback hell
+searchMovieAPI ('Lord of the Rings', () => {
+    saveToMyDB ('movies', () => {
+        // if it works, run this
+    }, () => {
+        // if it doesn't work, run this
+    })
+}, () => {
+    // if API is down or request failed, run this
+})
