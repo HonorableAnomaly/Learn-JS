150d34e71c861571d2272c740dab5fabaffa0f6e
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-01 20:16:03 -0800

    Update app.js
    +commented out fake callback code
    +const variable request equal to fakeRequestPromise(url)
    +request calling then through an arrow function and printing success statements or catch through an arrow function printing rejection statements
    +Refactored this syntax 2 other ways
    +fakeRequestPromise with an argument of url, without saving to a variable and chaining then and catch functions before reincorporating callback hell

--- ASync/Promises/app.js
+++ ASync/Promises/app.js
@@ -9,7 +9,8 @@ const fakeRequestCallback = (url, success, failure) => {
         }
     }, delay)
 }
-// THE PROMISE VERSION 
+
+// THE PROMISE VERSION - No need for success or failure arguments in the function
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * (4500)) + 500;
@@ -24,21 +25,72 @@ const fakeRequestPromise = (url) => {
 }
 
 
-fakeRequestCallback('books.com/page1', (response) => {
-    console.log("IT WORKED!!!");
-    console.log(response);
-    fakeRequestCallback('books.com/page2', (response) => {
-        console.log("IT WORKED AGAIN!!!");
-        console.log(response);
-        fakeRequestCallback('books.com/page3', (response) => {
-            console.log("IT WORKED AGAIN, AGAIN!!!");
-            console.log(response);
-        }, (err) => {
-            console.log("ERROR!!! (3rd request)", err);
-        })
-    }, (err) => {
-        console.log("ERROR!!! (2nd request)", err);
-    })
-}, (err) => {
-    console.log("ERROR!!!", err);
-})
+// fakeRequestCallback('books.com/page1', (response) => {
+//     console.log("IT WORKED!!!");
+//     console.log(response);
+//     fakeRequestCallback('books.com/page2', (response) => {
+//         console.log("IT WORKED AGAIN!!!");
+//         console.log(response);
+//         fakeRequestCallback('books.com/page3', (response) => {
+//             console.log("IT WORKED AGAIN, AGAIN!!!");
+//             console.log(response);
+//         }, (err) => {
+//             console.log("ERROR!!! (3rd request)", err);
+//         })
+//     }, (err) => {
+//         console.log("ERROR!!! (2nd request)", err);
+//     })
+// }, (err) => {
+//     console.log("ERROR!!!", err);
+// })
+
+
+// Calling the fakeRequestPromise with much simpler syntax than callback hell
+// const request = fakeRequestPromise('goodreads.com/api/fantasyreadinglist');
+// request.then(() => {
+//     console.log("PROMISE RESOLVED.");
+//     console.log("IT WORKED!!!");
+// }).catch(() => {
+//     console.log("PROMISE REJECTED.");
+//     console.log("OH NO, ERROR!!!");
+// })
+
+// Alternate way of spacing the code
+// const request = fakeRequestPromise('goodreads.com/api/fantasyreadinglist');
+
+// request
+//     .then(() => {
+//         console.log("IT WORKED!!!");
+//     }).catch(() => {
+//         console.log("OH NO, ERROR!!!");
+//     })
+
+
+// Chaining the variable and the request together!
+// fakeRequestPromise('goodreads.com/api/fantasyreadinglist')
+//     .then(() => {
+//         console.log("IT WORKED!!!");
+//     }).catch(() => {
+//         console.log("OH NO, ERROR!!!");
+//     });
+
+
+// Chaining the variable and the request together, and adding callback hell
+fakeRequestPromise('goodreads.com/api/fantasyreadinglist/page1')
+    .then(() => {
+        console.log("IT WORKED!!!");        
+        fakeRequestPromise('goodreads.com/api/fantasyreadinglist/page2')
+        .then(() => {
+            console.log("IT WORKED AGAIN!!!");            
+            fakeRequestPromise('goodreads.com/api/fantasyreadinglist/page3')
+            .then(() => {
+                console.log("IT WORKED AGAIN, AGAIN!!! ALL DATA RETRIEVED!!!");
+            }).catch(() => {
+                console.log("OH NO, ERROR!!! (3rd request)");
+            });
+        }).catch(() => {
+            console.log("OH NO, ERROR!!! (2nd request)");
+        });
+    }).catch(() => {
+        console.log("OH NO, ERROR!!!");
+    });
