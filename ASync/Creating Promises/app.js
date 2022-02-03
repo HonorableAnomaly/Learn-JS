a9144b2164485d3e8d41987cf2bc164b9f788a98
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-02 22:23:04 -0800

    Updates to two files
    index.html:
    +html style transition for the body color in 1 second intervals
    
    app.js
    +copied delayedColorChange code from previous lesson
    +Refactored above syntax using Promises, const variable delayedColorChange with two arguments color and delay through an arrow function returning a new Promise with two arguments resolve and reject through an arrow function containing a setTimeout with an arrow function that changes the background color to color and then resolves at delay
    +calling delayedColorChange inputting arguments color and delay, 6 identical then statements through arrow functions that call delayedColorChange inputting arguments color and delay

--- ASync/Creating Promises/app.js
+++ ASync/Creating Promises/app.js
@@ -23,3 +23,45 @@
 //   .catch((err) => {
 //     console.log("OH NO, AN ERROR!", err);
 //   });
+
+// A variable taking several arguments for the same setTimeout function
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = newColor;
+//     doNext && doNext();
+//   }, delay);
+// };
+
+// Rewritten function to take the color change variable, still nested, but better refactored
+// delayedColorChange("red", 1000, () => {
+//   delayedColorChange("orange", 1000, () => {
+//     delayedColorChange("yellow", 1000, () => {
+//       delayedColorChange("green", 1000, () => {
+//         delayedColorChange("blue", 1000, () => {
+//           delayedColorChange("indigo", 1000, () => {
+//             delayedColorChange("purple", 1000, () => {});
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+// Refactored older callback lesson with Promise
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+delayedColorChange("red", 1000)
+  .then(() => delayedColorChange("orange", 1000))
+  .then(() => delayedColorChange("yellow", 1000))
+  .then(() => delayedColorChange("green", 1000))
+  .then(() => delayedColorChange("blue", 1000))
+  .then(() => delayedColorChange("indigo", 1000))
+  .then(() => delayedColorChange("violet", 1000));
