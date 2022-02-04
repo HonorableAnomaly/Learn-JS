ff6012e606e93afadf4b12f792d63fd579b4054f
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-03 21:19:08 -0800

    Update app.js
    +parsing the JSON into JS
    +const variable dog with 4 key value pairs
    +stringifying dog into JSON
    +comment about the relic of XML

--- JSON/app.js
+++ JSON/app.js
@@ -1,1 +1,18 @@
+// This is a string of JSON saved to a variable
 const data = `{"ticker":{"base":"BTC","target":"USD","price":"443.7807865468","volume":"31720.1493969300","change":"0.3766203596"},"timestamp":1399490941,"success":true,"error":""}`;
+
+// We can parse it into JS with this
+JSON.parse(data);
+
+// This is a JS variable
+const dog = {
+  breed: "lab",
+  color: "black",
+  isAlive: true,
+  owner: undefined,
+};
+
+// We can turn the JS into JSON with this
+JSON.stringify(dog);
+
+// XML is pretty ancient now, and JSON is much more commonly used. So AJAX is more like AJAJ now
