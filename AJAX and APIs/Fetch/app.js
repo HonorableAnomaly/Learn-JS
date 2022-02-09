c0015dd2a54d0e66b70f0ec006273e132e46012e
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-08 19:22:19 -0800

    Update app.js
    +fetch called with a url, a then with res argument through an arrow function to print a message and res, returning the res.json method, another then with argument data through an arrow function to print a message and data and the score of data at the first index, and a catch with argument e through an arrow function that prints a message and e
    +const variable fetch TopGirlsMovieScore set to an implied async arrow function containing a try containing const variable res set to await fetch url and const variable data set to await res.json function and printing the score of data at the first index, and a catch with argument e that prints a message and e

--- AJAX and APIs/Fetch/app.js
+++ AJAX and APIs/Fetch/app.js
@@ -1,1 +1,24 @@
-fetch("https://api.cryptonator.com/api/ticker/btc-usd");
+// Promisy version of fetching an API
+// fetch("https://api.tvmaze.com/search/shows?q=girls")
+//   .then((res) => {
+//     console.log("RESPONSE, WAITING TO PARSE...", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log("DATA PARSED", data);
+//     console.log(data[0].score);
+//   })
+//   .catch((e) => {
+//     console.log("OH NO! ERROR!", e);
+//   });
+
+// ASYNC version of fetching an API
+const fetchTopGirlsMovieRating = async () => {
+  try {
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=girls");
+    const data = await res.json();
+    console.log(data[0].score);
+  } catch (e) {
+    console.log("OH NO! ERROR!", e);
+  }
+};
