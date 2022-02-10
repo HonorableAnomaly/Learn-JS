d634d4324125609a063e6569fa5f259341d7f958
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-09 20:09:38 -0800

    Updates to 2 files
    index.html:
    +two h1s
    
    app.js
    +commented out prototypes
    +copied over Colt's color class constructor object from another lesson
    +const variables navColor and logoColor set to new Color with names and rgb values as arguments
    +commented out color types
    +copied over hex color converter to rgb from Colt's code
    +commented out product code
    +factory function makeColor with 3 arguments r, g and b containing const variable color set to an empty object, r from color set to r, g from color set to g and b from color set to b, rgb from color set to a function containing const variable object r,g,b set to keyword this and returning string template literal calling r, g and b, and hex from color set to a function containing const variable object r,g,b set to keyword this and returning octothorpe in a string plus (code we haven't reviewed) set to string at base 16 and sliced at 1, all returning color
    +const variable firstColor set to makeColor with 3 arguments
    +product code from our makeColor function, commented out

--- OOP/app.js
+++ OOP/app.js
@@ -1,8 +1,72 @@
 // Adding a function to the String prototype, because we can
-String.prototype.yell = function () {
-  return `WOW!!! ${this.toUpperCase()}!!!!! AAAGHGHG!!!!`;
-};
+// String.prototype.yell = function () {
+//   return `WOW!!! ${this.toUpperCase()}!!!!! AAAGHGHG!!!!`;
+// };
 
-Array.prototype.pop = function () {
-  return "Sorry, I want that element. I will never pop it off!";
-};
+// Array.prototype.pop = function () {
+//   return "Sorry, I want that element. I will never pop it off!";
+// };
+
+// Colt's color class constructor object
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+}
+const red = new Color(255, 67, 89, "tomato");
+const white = new Color(255, 255, 255, "white");
+const navColor = new Color("carrot", [230, 126, 34]);
+const logoColor = new Color("emerald", [46, 204, 113]);
+
+// Color types
+// ("rgb(230, 34, 87)");
+// ("rgba(176, 52, 208, .3)");
+// ("#43FF0A");
+// ("hsl(29, 50%, 50%)");
+
+// Converts rgb colors to hex colors
+function hex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// Example of a hex that was produced from the rgb input
+// ("#cd1c76");
+// ("rgb(205, 28, 118)");
+
+// Built the color factory function that can convert rgb or hex into the other
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  };
+  color.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+  return color;
+}
+
+const firstColor = makeColor(35, 255, 150);
+// It worked!
+// #23ff96
