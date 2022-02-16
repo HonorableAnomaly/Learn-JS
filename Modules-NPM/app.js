69fcf17a6e9fb517efd63ba37524c82cb683700a
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-15 20:16:31 -0800

    Updates to two files
    math.js
    +4 different math equations set to variables, add, subtract, PI and square
    +each variable set to itself using module.exports
    +const variable object mathExports containing 4 key value pairs that are the math equation variables set to themselves
    +module.exports set to mathExports object
    +the 4 math equation variables set to module.exports directly
    +all previous code commented out
    +same syntax from above line refactored from module.exports to exports
    
    app.js
    +const variable math set to require the math filepath
    +printing PI of math and square with argument 9 of math
    +the above syntax destructured to call PI and square of 9 as arguments in the const variable

---
+++ Modules-NPM/app.js
@@ -0,0 +1,9 @@
+// requiring the implementation of another file
+const math = require("./math");
+console.log(math.PI);
+console.log(math.square(9));
+
+// destructured
+// const { PI, square } = require("./math");
+// console.log(PI);
+// console.log(square(9));
---
+++ Modules-NPM/math.js
@@ -0,0 +1,42 @@
+// math formula variables
+// const add = (x, y) => x + y;
+
+// const subtract = (x, y) => x - y;
+
+// const PI = 3.14159;
+
+// const square = (x) => x * x;
+
+// export modules
+// module.exports.add = add;
+// module.exports.subtract = subtract;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+// exports combined into a single object and exported on one line
+// const mathExports = {
+//   add: add,
+//   subtract: subtract,
+//   PI: PI,
+//   square: square,
+// };
+
+// module.exports = mathExports;
+
+// variables added onto exports directly
+// module.exports.add = (x, y) => x + y;
+
+// module.exports.subtract = (x, y) => x - y;
+
+// module.exports.PI = 3.14159;
+
+// module.exports.square = (x) => x * x;
+
+// refactored syntax
+exports.add = (x, y) => x + y;
+
+exports.subtract = (x, y) => x - y;
+
+exports.PI = 3.14159;
+
+exports.square = (x) => x * x;
