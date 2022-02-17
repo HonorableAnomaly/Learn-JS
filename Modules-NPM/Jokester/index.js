e7ddc34575f03fe7eb2fbc9961d19175ec4cadc8
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-16 19:01:16 -0800

    Installed package, linked to global, updates
    Installed package:
    +cowsay (global)
    
    Updates:
    Jokester\index.js
    +const variable cowsay set to require the cowsay package
    +linked cowsay to the directory via the terminal

--- Modules-NPM/Jokester/index.js
+++ Modules-NPM/Jokester/index.js
@@ -1,6 +1,8 @@
 // Requiring the entire npm package. It will search through the node_modules directory to find what it needs.
 const jokes = require("give-me-a-joke");
 const colors = require("colors");
+// Referencing a globally installed package
+const cowsay = require("cowsay");
 
 // To get a random Chuck Norris joke, incorporating the colors package with it
 jokes.getRandomCNJoke(function (joke) {
