541f60e15c55aaac51fd32e68da3b68fe2a6da69
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-17 22:52:21 -0800

    Updates to 2 files
    index.js
    +const variable path set to require it's module from node
    +res.render home file, used to send a file as a response to a request
    
    Bugs:
    -app.set with argument views and path.join with arguments dunder dirname and /views to redirect paths from anywhere outside of the dir to this dir
         -Updates to syntax likely required
    
    home.ejs
    +h1 The Home Page
    +2 paragraphs of lorem*3

--- Templating-Demo/index.js
+++ Templating-Demo/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+// A directory inside of express
+const path = require("path");
 
 app.set("view engine", "ejs");
+// Used to make the path to the views dir available from running our server anywhere, even outside of the dir where the folder is at.
+// Broken -- likely need to update syntax
+app.set("views", path.join(__dirname, "/views"));
 
+// Render allows us to 'send' a file as a response rather than just a string
 app.get("/", (req, res) => {
-  res.send("WELCOME TO THE HOMEPAGE!");
+  res.render("home");
 });
 
 app.listen(3000, () => {
