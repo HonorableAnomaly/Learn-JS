b358d09029203a7b2bac1a4f878137b20b00d5d4
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-17 20:42:11 -0800

    Global package installed, updates
    Installed:
    +nodemon
    
    Updates:
    FirstApp/index.js
    +commented out initial request
    +get request created for the root path that logs a string and sends an h1 response
    +get request created for a specified base path and a param and two arguments req and res through an arrow function, const variable object using the param as an argument set to req.params, logs req.params, logs a string and sends a string template literal h1 referencing the param
    +get request created for a specified path and two params and two arguments req and res through an arrow function, const variable object using the two params as arguments set to req.params, logs req.params, logs a string and sends a string template literal h1 referencing both params
    +3 get requests created each for a specified path and two arguments req and res through an arrow function, logs a string and sends a string h1
    +post request created for a specified path and two arguments req and res through an arrow function, logs a string and sends a string h1
    +get request with the path * and two arguments req and res through an arrow function, logs a string and sends a string h1 used as a catch-all for invalid input that isn't coded
    +comments added

--- Express-Intro/FirstApp/index.js
+++ Express-Intro/FirstApp/index.js
@@ -1,11 +1,72 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST!");
-  //   res.send("HELLO! WE GOT YOUR REQUEST! THIS IS A RESPONSE!");
-  //   res.send({ color: "blue" });
-  res.send("<h1>This is my webpage!</h1>");
+// req is an object created by express, and while res is too, it serves as the response object the server returns
+
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST!");
+//   //   res.send("HELLO! WE GOT YOUR REQUEST! THIS IS A RESPONSE!");
+//   //   res.send({ color: "blue" });
+//   res.send("<h1>This is my webpage!</h1>");
+// });
+
+// Root route get request is just a slash
+app.get("/", (req, res) => {
+  console.log("YOU'VE CALLED THE HOMEPAGE!");
+  res.send("<h1>WELCOME HOME!</h1>");
 });
 
+// Pattern matching using the req as input
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  console.log(req.params);
+  console.log("YOU'VE REQUESTED A SUBREDDIT!");
+  res.send(`<h1>Welcome to the ${subreddit} subreddit!</h1>`);
+});
+
+// Further nesting of paths
+app.get("/r/:subreddit/:postId", (req, res) => {
+  const { subreddit, postId } = req.params;
+  console.log(req.params);
+  console.log("YOU'VE REQUESTED A SUBREDDIT!");
+  res.send(`<h1>Viewing Post ID:${postId} on the ${subreddit} subreddit!</h1>`);
+});
+
+// Examples of separate paths for responses
+app.get("/bulbasaur", (req, res) => {
+  console.log("YOUR BULBASAUR HEARD YOUR CALL!");
+  res.send("<h1>BULBA BULBA BULBASAUR!</h1>");
+});
+
+app.get("/charmander", (req, res) => {
+  console.log("YOUR CHARMANDER HEARD YOUR CALL!");
+  res.send("<h1>CHARMANDER CHAR!!</h1>");
+});
+
+app.post("/charmander", (req, res) => {
+  console.log("YOUR CHARMANDER HEARD YOUR CALL!");
+  res.send("<h1>CHARMANDER CHAR!!</h1> This is different than a get request!");
+});
+
+app.get("/squirtle", (req, res) => {
+  console.log("YOUR SQUIRTLE HEARD YOUR CALL!");
+  res.send("<h1>SQUIRTLE SQUIR SQUIRTLE!</h1>");
+});
+
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    res.send("<h1>Your search for nothing has produced... well... nothing.</h1>");
+  } else {
+    res.send(`<h1>Search results for ${q} were a success!</h1>`);
+  }
+});
+
+// The star is a catch-all for invalid requests that weren't coded or would otherwise fail
+app.get("*", (req, res) => {
+  console.log("AN INVALID REQUEST HAS BEEN MADE!");
+  res.send("<h1>I'M SORRY, BUT WE DON'T HAVE THAT HERE! TRY CALLING A POKéMON'S NAME!</h1>");
+});
+
+// Listener that enables the server
 app.listen(3000, () => [console.log("LISTENING ON PORT 3000!")]);
