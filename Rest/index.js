df21dc242d2c22f15ed4ec76ac40747c1a3960ad
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-21 11:53:39 -0800

    Update index.js
    +get request with address argument, sending a string
    +post request with address argument, sending a string

--- Rest/index.js
+++ Rest/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("http://localhost:3000/tacos", (req, res) => {
+  res.send("GET /tacos response!");
+});
+
+app.post("http://localhost:3000/tacos", (req, res) => {
+  res.send("POST /tacos response!");
+});
+
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000...");
 });
