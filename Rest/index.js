6efbd799f744eb95d3a23722c514cc233b540885
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-21 14:33:31 -0800

    New file, updates
    New file:
    +index.ejs
    
    Updates:
    index.js
    +requiring path
    +app.setting the view engine to ejs
    +app.setting views to path.join with arguments dunder dirname and the views dir
    +const array comments containing 4 objects that each have 2 key value pairs username and comment
    +app.getting path / and res.sending string
    +app.getting path comments and res.rendering comments/index template with the object comments
    
    index.ejs
    +boilerplate html
    +ejs for of loop c of comments that creates an li within the ul and calls c.comment and c.username

--- Rest/index.js
+++ Rest/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const comments = [
+  {
+    username: "Ricky",
+    comment: "Hi, my name is Ricky!",
+  },
+  {
+    username: "sk8erBoi",
+    comment: "Plz deletee ur account Ricky.",
+  },
+  {
+    username: "RandoPando",
+    comment: "No one around here nice eh?",
+  },
+  {
+    username: "Truth-Unveiled",
+    comment: "Not here. Or anywhere. All inherently evil they are.",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
 app.get("http://localhost:3000/tacos", (req, res) => {
   res.send("GET /tacos response!");
