bb0a6a4f1f2ad1f8feffef85a82699ba15f3cf44
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-21 14:03:34 -0800

    Installed package and updates
    Installed:
    +ejs
    
    Updates:
    index.js
    +app.use urlencoded and json data for express

--- Rest/index.js
+++ Rest/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("http://localhost:3000/tacos", (req, res) => {
   res.send("GET /tacos response!");
 });
 
 app.post("http://localhost:3000/tacos", (req, res) => {
-  res.send("POST /tacos response!");
+  const { meat, qty } = req.body;
+  res.send(`Okay! Here are your ${qty} ${meat} tacos! Enjoy!`);
 });
 
 app.listen(3000, () => {
