d2a32b0d18a93cbdf97a72bf9ee8b14621dbfbb9
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-21 16:50:31 -0800

    New files, installed package, updates
    New files:
    +new.ejs
    +show.ejs
    
    Installed:
    +npm uuid
    
    Updates:
    index.js
    +requiring uuid
    +hardcoded uuids into fake database array objects
    +app.getting path comments/new with arguments req and res through an arrow function and res.rendering new ejs template
    +app.posting path comments with arguments req and res through an arrow function containing const variable destructured object username and comment set to req.body, comments.push the new object with username, comment and uuid, res.redirecting to path comments
    +app.getting path comments:id with arguments req and res through an arrow function containing const variable destructured id set to req.params, const variable comment set to comments.find function c through an arrow function c.id strictly equal to id and res.redirecting to path comments/show with argument object comment
    +app.patching path comments:id with arguments req and res through an arrow function containing const variable destructured id set to req.params, const variable updatedComment set to req.body.comment, const foundComment set to comments.find function c through an arrow function c.id strictly equal to id, changing foundComment.comment to updatedComment and res.redirecting to path comments
    +commented out tacos code
    
    index.ejs
    +anchor tag link that redirects to path /comments ejs c.id called details
    +anchor tag link that redirects to path /comments/new called Post a new comment
    
    new.ejs
    +boilerplate html
    +h1
    +post form with 2 labels and an input and a textarea, one for username and one for comment
    +submit button
    +anchor tag link to path comments
    
    show.ejs
    +boilerplate html
    +h1 with comment ID and ejs comment.id
    +h2 with ejs comment.comment and comment.username
    +anchor tag link to path /comments

--- Rest/index.js
+++ Rest/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,18 +10,22 @@ app.set("views", path.join(__dirname, "views"));
 
 const comments = [
   {
+    id: uuid(),
     username: "Ricky",
     comment: "Hi, my name is Ricky!",
   },
   {
+    id: uuid(),
     username: "sk8erBoi",
     comment: "Plz deletee ur account Ricky.",
   },
   {
+    id: uuid(),
     username: "RandoPando",
     comment: "No one around here nice eh?",
   },
   {
+    id: uuid(),
     username: "Truth-Unveiled",
     comment: "Not here. Or anywhere. All inherently evil they are.",
   },
@@ -34,14 +39,39 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
 
-app.get("http://localhost:3000/tacos", (req, res) => {
-  res.send("GET /tacos response!");
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
 });
 
-app.post("http://localhost:3000/tacos", (req, res) => {
-  const { meat, qty } = req.body;
-  res.send(`Okay! Here are your ${qty} ${meat} tacos! Enjoy!`);
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
 });
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedComment = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = updatedComment;
+  res.redirect("/comments");
+});
+
+// Basic get and post
+// app.get("http://localhost:3000/tacos", (req, res) => {
+//   res.send("GET /tacos response!");
+// });
+
+// app.post("http://localhost:3000/tacos", (req, res) => {
+//   const { meat, qty } = req.body;
+//   res.send(`Okay! Here are your ${qty} ${meat} tacos! Enjoy!`);
+// });
 
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000...");
