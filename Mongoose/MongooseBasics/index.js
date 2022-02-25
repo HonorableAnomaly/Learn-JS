f14d7f47eab02effbc7e916348baefc899ac6c90
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-24 23:21:48 -0800

    Update index.js
    +new Schema gameSchema with 4 key value pairs
    +class Game set to mongoose model with argument Game using gameSchema
    +new Game with unique values for the 4 key value pairs

--- Mongoose/MongooseBasics/index.js
+++ Mongoose/MongooseBasics/index.js
@@ -1,3 +1,4 @@
+// Requiring mongoose and database connection
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/gameApp")
@@ -8,3 +9,16 @@ mongoose
     console.log("OH NO! MONGOOSE HAS HIDDEN ITSELF!");
     console.log(err);
   });
+
+//  Mongoose schema setup for database
+const gameSchema = new mongoose.Schema({
+  title: String,
+  release: Number,
+  genre: String,
+  platform: String,
+});
+
+// Class that allows for the creation of new instances of the class that can be saved to the database
+// When called, creates an instance of the class and creates the collection in the database
+const Game = mongoose.model("Game", gameSchema);
+const donkeykongcountry = new Game({ title: "Donkey Kong Country", release: 1994, genre: "platform", platform: "Super Nintendo" });
