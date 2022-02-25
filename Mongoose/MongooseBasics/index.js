293bfb65310258af1a601039ac917f833a3d4249
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-24 21:51:33 -0800

    Update index.js
    +requiring Mongoose
    +mongoose.connect with argument database link
    +.then function that logs when connected
    +.catch err and logs when an error occurs, logging err

---
+++ Mongoose/MongooseBasics/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/gameApp")
+  .then(() => {
+    console.log("MONGOOSE SAW YOU!");
+  })
+  .catch((err) => {
+    console.log("OH NO! MONGOOSE HAS HIDDEN ITSELF!");
+    console.log(err);
+  });
