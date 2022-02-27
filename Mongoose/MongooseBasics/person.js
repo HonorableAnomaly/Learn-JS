0a7682510566f212af48b59c9fcd5738e241fec8
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-02-26 21:50:18 -0800

    Update person.js
    +personSchema pre save function that logs a string
    +personSchema post save function that logs a string

--- Mongoose/MongooseBasics/person.js
+++ Mongoose/MongooseBasics/person.js
@@ -19,4 +19,13 @@ personSchema.virtual("fullName").get(function () {
   return `${this.first} ${this.last}`;
 });
 
+// Middleware functions
+personSchema.pre("save", async function () {
+  console.log("ABOUT TO SAVE!!!");
+});
+
+personSchema.post("save", async function () {
+  console.log("JUST SAVED!!!");
+});
+
 const Person = mongoose.model("Person", personSchema);
