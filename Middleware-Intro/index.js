c9ef0736b08fd7366da8e3b8e43e85f5480b42c2
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-03-15 19:01:49 -0700

    Middleware routing
    +3 app.uses with 3 arguments req, res and next through arrow functions, logging strings and calling method next to demo how middleware operates

--- Middleware-Intro/index.js
+++ Middleware-Intro/index.js
@@ -3,6 +3,21 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log("THIS IS MY FIRST MIDDLEWARE!");
+  next();
+  console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()");
+});
+
+app.use((req, res, next) => {
+  console.log("THIS IS MY SECOND MIDDLEWARE!");
+  return next();
+});
+
+app.use((req, res, next) => {
+  console.log("THIS IS MY THIRD MIDDLEWARE!");
+  return next();
+});
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE!");
