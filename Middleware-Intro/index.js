7caca1e99cd1da16ff815c1d941b855aec3b0b35
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-03-15 19:27:01 -0700

    Middleware practice with Date.now()
    +req.requestTime set to Date.now method and logging req.method and req.path added to app.use
    +commented out demo middleware
    +logging string template literals with req.requestTime in template paths

--- Middleware-Intro/index.js
+++ Middleware-Intro/index.js
@@ -3,27 +3,39 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(morgan("dev"));
+
+// The order that we place middleware matters for execution!
 app.use((req, res, next) => {
-  console.log("THIS IS MY FIRST MIDDLEWARE!");
+  // req.method = "GET";
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
   next();
-  console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()");
 });
 
-app.use((req, res, next) => {
-  console.log("THIS IS MY SECOND MIDDLEWARE!");
-  return next();
-});
+// Demo middleware
+// app.use((req, res, next) => {
+//   console.log("THIS IS MY FIRST MIDDLEWARE!");
+//   next();
+//   console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()");
+// });
 
-app.use((req, res, next) => {
-  console.log("THIS IS MY THIRD MIDDLEWARE!");
-  return next();
-});
+// app.use((req, res, next) => {
+//   console.log("THIS IS MY SECOND MIDDLEWARE!");
+//   return next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("THIS IS MY THIRD MIDDLEWARE!");
+//   return next();
+// });
 
 app.get("/", (req, res) => {
+  console.log(`REQUEST TIME: ${req.requestTime}`);
   res.send("HOME PAGE!");
 });
 
 app.get("/dogs", (req, res) => {
+  console.log(`REQUEST TIME: ${req.requestTime}`);
   res.send("WOOF WOOF!");
 });
 
