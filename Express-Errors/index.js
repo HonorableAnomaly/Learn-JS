bc1a4796599dc6ca2eb7b112d3de2bfab1f60f03
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-03-17 20:45:02 -0700

    New file 'AppError.js' and error handlers
    AppError.js
    +Class AppError extending Error functionality, a constructor with 2 arguments status and message, using method super and setting this.status to status and this.message to message
    
    index.js
    +Requiring AppError
    +Comments added
    +app.get path '/admin' to throw AppError with 403 status and error string
    +Commented out original error handlers
    +app.use error handler examples using destructured status codes with default of 500 set to err
    +One handler utilizing res.status with argument status and .sending an error string
    +Another handler utilizing destructured message with default error string set to err and res.status with argument status .sending argument message
    +Final example puts both destructured status and message on one line

--- Express-Errors/index.js
+++ Express-Errors/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const AppError = require("./AppError");
+
 app.use(morgan("dev"));
 
 // The order that we place middleware matters for execution!
@@ -17,38 +19,22 @@ app.use("/dogs", (req, res, next) => {
   next();
 });
 
-// Defining a middleware to be used as a function on another middleware
+// Thrown error
 const verifyPassword = (req, res, next) => {
   const { password } = req.query;
   if (password === "chickennugget") {
     next();
   }
   // res.send("SORRY, YOU NEED A PASSWORD!");
-  throw new AppError(401, "UH OH! ERROR!");
+  throw new AppError(401, "Password required");
 };
-
-// Demo middleware
-// app.use((req, res, next) => {
-//   console.log("THIS IS MY FIRST MIDDLEWARE!");
-//   next();
-//   console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()");
-// });
-
-// app.use((req, res, next) => {
-//   console.log("THIS IS MY SECOND MIDDLEWARE!");
-//   return next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log("THIS IS MY THIRD MIDDLEWARE!");
-//   return next();
-// });
 
 app.get("/", (req, res) => {
   console.log(`REQUEST TIME: ${req.requestTime}`);
   res.send("HOME PAGE!");
 });
 
+// Thrown syntax error
 app.get("/error", (req, res) => {
   chicken.fly();
 });
@@ -63,22 +49,45 @@ app.get("/secret", verifyPassword, (req, res, next) => {
   res.send("MY SECRET IS: I REALLY LIKE POKéMON!");
 });
 
-// Last ditch response if nothing else works
+app.get("/admin", (req, res) => {
+  throw new AppError(403, "You are not an Admin!");
+});
+
+//
+// Typical last ditch error response without being an error handler
 app.use((req, res) => {
   res.status(404).send("Whatever you may be seeking, we know it isn't that. We don't have any of that.");
 });
 
 // Error handlers must be last!
+//
+// app.use((err, req, res, next) => {
+//   console.log("**************************************");
+//   console.log("**************ERROR*******************");
+//   console.log("**************************************");
+//   console.log(err);
+// One option for error handling
+// res.status(500).send("OH BOY, AN ERROR!");
+//
+// Calls the next error-handling middleware
+//   next(err);
+// });
+
+// Three ways to handle errors without error class
+// app.use((err, req, res, next) => {
+//   const { status = 500 } = err;
+//   res.status(status).send("ERROOORORRRRR!!!");
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500 } = err;
+//   const { message = "ERROOORORRRRR!!!" } = err;
+//   res.status(status).send(message);
+// });
+
 app.use((err, req, res, next) => {
-  console.log("**************************************");
-  console.log("**************ERROR*******************");
-  console.log("**************************************");
-  console.log(err);
-  // One option for error handling
-  // res.status(500).send("OH BOY, AN ERROR!");
-  //
-  // Calls the next error-handling middleware
-  next(err);
+  const { status = 500, message = "ERROOORORRRRR!!!" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
