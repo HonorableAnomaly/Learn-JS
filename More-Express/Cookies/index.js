5f1a74e623770cdacbd6e3e68b22d9682707ca7a
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-04-06 20:23:20 -0700

    Using cookie-parser
    index.js
    +Requiring cookie-parser
    +Destructuring name with a default from req.cookies
    +Res.sending string template literal with name
    +Changed name value in res.cookie

--- More-Express/Cookies/index.js
+++ More-Express/Cookies/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 app.get("/greet", (req, res) => {
-  res.send("Hey there!");
+  const { name = "No-Name" } = req.cookies;
+  res.send(`Hey there, ${name}!`);
 });
 
 app.get("/setname", (req, res) => {
-  res.cookie("name", "stevie chicks");
+  res.cookie("name", "Toddicus");
   res.cookie("animal", "harlequin shrimp");
   res.send("Okay, sent you a cookie!");
 });
