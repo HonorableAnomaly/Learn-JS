da30e358cc0ea1b64911cec7869252713d726e30
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-04-18 21:22:24 -0700

    Registration
    New file:
    Register.ejs
    +EJS template with a form that contains username and password inputs and a button
    
    user.js
    +Requiring mongoose
    +A userSchema model used to create user objects
    
    index.js
    +Requiring express, mongoose, User model, path, bcrypt, app for express
    +Mongoose connection to 'authDemo' db
    +App.set for view engine ejs and views path
    +App.use for express urlencoded
    +App.get for '/' and '/register'
    +App.post for '/register' including hashed bcrypt password and salt, creating a new user object with arguments username and hashed password, saving the user and res.redirecting to '/'
    +App.get for '/secret'
    +App.listen on port 3000

---
+++ Auth/Auth-Demo/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+mongoose.connect("mongodb://localhost:27017/authDemo"),
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  };
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected...");
+});
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("THIS IS THE HOME PAGE!");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
+});
+
+app.get("/secret", (req, res) => {
+  res.send("THIS IS SECRET! YOU CANNOT SEE ME UNLESS LOGGED IN!");
+});
+
+app.listen(3000, () => {
+  console.log("Serving on Port 3000...");
+});
